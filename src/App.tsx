70dc3c9ef6d9d94a1930b27c698a730157e5e51e
GER-ITL <danteryzvelt@inbox.ru>
2024-04-29 10:11:50 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,8 @@
-import { Link } from 'react-router-dom'
-import reactLogo from './assets/react.svg'
-import './styles/App.scss'
-import viteLogo from '/vite.svg'
 function App() {
 	return (
 		<>
 			<div>
-				<Link to='/next'>
-					<a href='https://vitejs.dev' target='_blank'>
-						<img src={viteLogo} className='logo' alt='Vite logo' />
-					</a>
-				</Link>
-				<a href='https://react.dev' target='_blank'>
-					<img src={reactLogo} className='logo react' alt='React logo' />
-				</a>
+				<h1>Email EDITOR</h1>
 			</div>
 		</>
 	)
