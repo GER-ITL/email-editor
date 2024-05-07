68a86d98698dee3a2d4bdfdd479d397d0fd98482
GER-ITL <danteryzvelt@inbox.ru>
2024-05-07 09:52:27 +0300

    ts

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import EmailList from './components/email-list/EmailList'
+import App from './pages/home/App'
 import Provider from './provider'
-import router from './router/Router'
 import './styles/index.scss'
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />,
+		errorElement: <h1>ERROR 404</h1>,
+	},
+	{
+		path: '/messages',
+		element: <EmailList />,
+	},
+])
 
 const rootElement = document.getElementById('root')
 if (!rootElement) throw new Error('Failed root')
